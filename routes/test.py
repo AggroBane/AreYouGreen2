2f4e523f19363637b7406c6e2a05d27707e9c87f
Andre David Garcia Saenz <a.d.garcia@edu.cegepgarneau.ca>
2021-01-30 12:56:21 -0500

    Log in page + some bootstrap in index

--- routes/test.py
+++ routes/test.py
@@ -2,4 +2,4 @@ from . import routes
 from flask import Flask, render_template
 @routes.route("/test")
 def test():
-    return render_template('index.html')
+    return render_template('login.html')
