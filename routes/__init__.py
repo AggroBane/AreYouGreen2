80f5e2cc302960348bb4da31343f5d6c41ec2768
Andre David Garcia Saenz <a.d.garcia@edu.cegepgarneau.ca>
2021-01-30 14:16:24 -0500

    Added Get to routers

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,3 +3,4 @@ routes = Blueprint('routes', __name__)
 
 from .index import *
 from .test import *
+from .organisation import *
--- routes/organisation.py
+++ routes/organisation.py
@@ -1,9 +1,11 @@
 from . import routes
 from flask import Flask, render_template, request
-@routes.route("/test")
-def test():
+
+
+@routes.route("/organisation/<id>")
+
+def orgInfo(id):
 
     name = request.args.get('name')
 
     return name
-    #return render_template('organisation.html')
