3c127cae3f84d642e1d1a775a21d4fbf46f4fc1e
AggroBane <william-fecteaurhotmail.com>
2021-01-31 07:07:41 -0500

    Renamed user somewhere

--- ModelImports.py
+++ ModelImports.py
@@ -1,5 +1,5 @@
 from models.Organisations import *
 from models.Departments import *
-from models.Users import *
+from models.users import *
 from models.Tasks import *
 from models.Collabs import *
