bcf71c2240283f631707e1f7d2a4bb8e010e9eef
Jordan G <itsseraphii@gmail.com>
2021-01-31 00:00:01 -0500

    Added owner model

--- models/tasks.py
+++ models/tasks.py
@@ -3,7 +3,7 @@ class Tasks(db.Model):
     name = db.Column(db.String(50), nullable = False)
     description = db.Column(db.String(128), nullable = True)
     checked = db.Column(db.Boolean, default=False)
-    fk_assignedUser = db.Column(db.String(50), db.ForeignKey('user.username'))
-    fk_department = db.Column(db.String(50), db.ForeignKey('department.name'))
+    fk_assignedUser = db.Column(db.String(50), db.ForeignKey('users.username'))
+    fk_department = db.Column(db.String(50), db.ForeignKey('departments.name'))
     collaborators = db.relationship('Collabs', backref='task')
 
--- models/users.py
+++ models/users.py
@@ -1,6 +1,5 @@
 class Users(db.Model):
     username = db.Column(db.String(50), nullable = False, primary_key = True)
-    fk_department = db.Column(db.String(50), db.ForeignKey('department.name'), nullable=True)
-    organisations = db.relationship('Organisations', backref='user')
+    fk_department = db.Column(db.String(50), db.ForeignKey('departments.name'), nullable=True)
     tasks = db.relationship('Tasks', backref='user')
     tasks = db.relationship('Collabs', backref='user')
