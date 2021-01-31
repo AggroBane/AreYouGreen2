d3ccda76978e4d740e9641bf04642bc5cd4d9540
Jordan G <itsseraphii@gmail.com>
2021-01-31 08:56:39 -0500

    Tried to do signup

--- routes/signup.py
+++ routes/signup.py
@@ -1,14 +1,21 @@
 from . import routes
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from app import db
 from ModelImports import *
 
-@routes.route("/create")
+@routes.route("/create",  methods=['GET', 'POST'])
 def orgCreate():
-    # Create new organisation!
-    # TODO Create signup page => redirect to create/orgId
-    return
+    if request.method == 'GET':
+        return render_template("register.html")
+    else :
+        # Create org in database
+        desiredName = request.form.get('orgId')
+        print(desiredName)
+        newOrg = Organisations(name = desiredName)
+        db.session.add(newOrg)
+        db.session.commit()
+        return redirect('/create/' + request.form.get('orgId'))
 
 @routes.route("/create/<orgId>")
 def orgJoin(orgId):
